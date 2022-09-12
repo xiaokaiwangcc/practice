c4f838dbfedf645aecb072798b131bdfc7def4e3
karry <wangxiaokaismile@163.com>
2022-09-12 14:57:22 +0800

    09-12 宏任务与微任务

--- src/Js/EventLoop/macro-mincro.js
+++ src/Js/EventLoop/macro-mincro.js
@@ -20,3 +20,53 @@ setTimeout(() => {
     console.log('length2', $('#container').children().length)
     alert('setTimeout') // dom渲染了
 })
+
+new Promise((resolve)=> {
+    resolve(Promise.resolve(5))
+    console.log(4)
+    resolve(Promise.resolve(5))
+}).then(res => {
+    console.log('res', res)
+})
+
+// async function fn1() {
+//     console.log(1) // 1
+//     await fn2()
+//     console.log(2) //4
+// }
+//
+// async function fn2() {
+//     console.log('fn2') //2
+// }
+// fn1()
+// console.log(3) //3
+//
+//
+//
+// console.log('script start');
+//
+// setTimeout(function() {
+//     console.log('setTimeout1');
+//     Promise.resolve().then(function() {
+//     }).then(function() {
+//         console.log('promise5');
+//         setTimeout(function() {
+//             console.log('setTimeout3');
+//         }, 0);
+//     });
+// }, 0);
+//
+// Promise.resolve().then(function() {
+//     console.log('promise1');
+// }).then(function() {
+//     console.log('promise2');
+//     Promise.resolve().then(function() {
+//         console.log('promise3');
+//     }).then(function() {
+//         console.log('promise4');
+//         setTimeout(function() {
+//             console.log('setTimeout2');
+//         }, 0);
+//     });
+// });
+// console.log('script end');
